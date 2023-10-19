60bde59df2e540e2cc5c2c1da0b06d009e85b459
stronggeniusa <stronggeniusa@gmail.com>
2023-10-19 10:53:39 +0700

    Insert

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # russian-cities-mongodb
+## npm start
+ORIGINAL: [russian-cities](https://github.com/pensnarik/russian-cities/tree/master)
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { MongoClient, ObjectId } from 'mongodb'
+import Cities from './russian-cities.json'
 
 Action().then().catch()
 
@@ -6,8 +7,27 @@ async function Action () {
     const client = new MongoClient('mongodb://127.0.0.1:27017/?authSource=admin')
     await client.connect()
     let mongoClient =  client.db('russian-cities-mongodb')
-    let collection = mongoClient.collection('user')
+    let collectionSubject = mongoClient.collection('subject')
+    let collectionCity = mongoClient.collection('city')
 
+    for (let item of Cities) {
 
+        let subject = await collectionSubject.findOne({name: item.subject})
+        if (!subject) {
+            subject = {
+                name: item.subject
+            }
+            await collectionSubject.insertOne(subject)
+        }
+
+        let city = await collectionCity.findOne({name: item.name, subject: item.subject})
+        if (!city) {
+            item.subject_id = subject._id
+            await collectionCity.insertOne(item)
+        }
+
+    }
+
+    console.log('Готово')
 }
 
